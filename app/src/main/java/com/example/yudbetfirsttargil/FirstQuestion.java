e388ebea0734af41c84e565794d87e78b23392d6
Roni Lev <roniilevv@gmail.com>
2022-11-28 13:05:57 +0200

    editing the first question

--- app/src/main/java/com/example/yudbetfirsttargil/FirstQuestion.java
+++ app/src/main/java/com/example/yudbetfirsttargil/FirstQuestion.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 
 public class FirstQuestion extends AppCompatActivity {
 
@@ -16,5 +17,22 @@ public class FirstQuestion extends AppCompatActivity {
     public void nextQuestion (View view) {
         Intent intent = new Intent(this, SecondQuestion.class);
         startActivity(intent);
+    }
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch(view.getId()) {
+            case R.id.first_option:
+                if (checked)
+                    break;
+            case R.id.second_option:
+                if (checked)
+                    break;
+            case R.id.third_option:
+                if (checked)
+                    break;
+            case R.id.forth_option:
+                if (checked)
+                    break;
+        }
     }
 }
